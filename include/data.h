254a8840ae84214fb0bde078a96c4cc4e21c39a3
epitech2020 <baptiste.heraud@epitech.eu>
2016-06-07 17:54:09 +0200

    init

---
+++ include/data.h
@@ -0,0 +1,11 @@
+
+#ifndef DATA_H_
+#define DATA_H_
+
+#define	LEN 4096
+
+		/* main.c */
+
+int		 main(void);
+
+#endif /* DATA_H_ */
---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+** main.c for  in /home/baptiste/project/generator_h
+**
+** Made by
+** Login   <baptiste@epitech.net>
+**
+** Started on  Mon May 16 10:48:51 2016
+** Last update Mon Jun  6 13:34:37 2016 
+*/
+
+#include "data.h"
+
+int		main(void)
+{
+  char	buff[LEN];
+
+  return (1);
+}
